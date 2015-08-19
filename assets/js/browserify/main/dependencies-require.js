c062e18ceb1a879d093af2c85c48b8bc49b85161
Maximiliano Benedetto <infoprisma1@gmail.com>
2015-08-19 17:11:27 +0200

    Change libs to their own file

--- assets/js/browserify/main/dependencies-require.js
+++ assets/js/browserify/main/dependencies-require.js
@@ -1,3 +1,5 @@
-var uiRouter = require('angular-ui-router');
-var angular = require('angular');
+//weload angular and angular ui from thir sole file, in order to allow caching beyond this pacakge's lifespan
+// var angular = require('angular');
+// var uiRouter = require('angular-ui-router');
+
 var login = require("../../modules/login");
